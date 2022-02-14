601428131e4acc44001685736f0c75c8f801b74d
iamvahid <vahidalizadeh7070@gmail.com>
2022-02-15 00:09:36 +0330

    Add Mvc instance to handle exceptions

---
+++ MVCApplication_ErrorHandling_Singleton/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using MVCApplication_ErrorHandling.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCApplication_ErrorHandling.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ILog _ILog;
+
+        public HomeController()
+        {
+            _ILog = Log.GetInstance;
+        }
+
+        // We override OnException method to handle all error that we have get from our controller.
+        // After we override it, we do not need try catch block any more 
+        // If we have any exception in our code, this method will be executed
+        protected override void OnException(ExceptionContext filterContext)
+        {
+            _ILog.LogException(filterContext.Exception.ToString());
+            filterContext.ExceptionHandled = true;
+            this.View("Error").ExecuteResult(this.ControllerContext);
+        }
+        public ActionResult Index()
+        {
+            // We use it to throw in exception and execute OnException method
+            int number = 10;
+            var error=number / 0;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MVCApplication_ErrorHandling_Singleton/Models/Log.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace MVCApplication_ErrorHandling.Models
+{
+    // We use sealed keyword due to the using of private Log constructor
+    public sealed class Log : ILog
+    {
+        // private constructor
+        private Log()
+        { 
+
+        }
+
+        // Singleton design pattern to create and instance from Log class
+        private static readonly Lazy<Log> instance = new Lazy<Log>(()=>new Log());
+        public static Log GetInstance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        // This method handle the log exception
+        public void LogException(string message)
+        {
+            string fileName = string.Format("{0}_{1},Log","Exception",Guid.NewGuid().ToString());
+            string logFilePath = string.Format(@"{0}\{1}",AppDomain.CurrentDomain.BaseDirectory,fileName);
+
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("-------");
+            stringBuilder.AppendLine(DateTime.Now.ToString());
+            stringBuilder.AppendLine(message);
+            using (StreamWriter writer = new StreamWriter(logFilePath,true))
+            {
+                writer.Write(stringBuilder.ToString());
+                writer.Flush();
+            }
+        }
+    }
+}
